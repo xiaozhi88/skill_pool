d74b2bec22b7460c89b25382cf4c73cf5adf608a
zhangzy <123456>
2021-05-10 15:46:54 +0800

    示例类

---
+++ 动态代理/src/main/java/com/xiaozhi/ProxyDemo1.java
@@ -0,0 +1,17 @@
+package com.xiaozhi;
+
+/***************************************************/
+  /* 
+  Usage                : 示例类
+  Project name         : skill_pool
+  Author               : zhangzy
+  Mail                 : zhangzy@chinaexpressair.com
+  Date                 : 2021-05-10 15:45
+  Version              : 1.0
+  Modification history :
+      Date          Author          Version          Description
+  ---------------------------------------------------------------
+   2021-05-10       zhangzy             1.0             新建
+   @Copyright(C)2008-2020: 华夏航空股份有限公司 All right Reserved.
+  */public class ProxyDemo1 {
+}
